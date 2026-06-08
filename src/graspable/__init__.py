4743c8396c8b32417b703dcc1736c1d6aaf1d061
Maja Maschke <80268744+maj-mas@users.noreply.github.com>
2026-06-08 16:28:30 +0200

    update module file

--- src/graspable/__init__.py
+++ src/graspable/__init__.py
@@ -5,3 +5,7 @@ __author__ = "Maja Maschke"
 from .main import GraspableMain
 from .calculation import Calculation
 from .environment import Environment
+from .nuclear import Nuclear
+from .csfmanager import CSFManager
+from .scf import SelfConsistentField
+from .optimisation_strategy import AbstractOptimisationStrategy, TestStrategy
