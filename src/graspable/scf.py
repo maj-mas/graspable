163dbc028409d45d18b8208ecc3c8800713e3321
Maja Maschke <80268744+maj-mas@users.noreply.github.com>
2026-07-29 12:19:54 +0200

    facepalm

--- src/graspable/scf.py
+++ src/graspable/scf.py
@@ -68,7 +68,7 @@ class SelfConsistentField:
         Returns:
             _type_: Space delimited string of orbitals with wild card appended.
         """
-        if len(orbitals) == 1 and orbitals[0] == "":
+        if orbitals == "":
             return orbitals
         if "*" in orbitals:
             return orbitals
